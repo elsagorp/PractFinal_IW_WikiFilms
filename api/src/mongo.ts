7e403c6c1baed5419e1f5f511a4dd5b43d86a60c
elsagorp <egordillo@alumnos.nebrija.es>
2022-05-26 17:59:43 +0200

    commit

--- api/src/mongo.ts
+++ api/src/mongo.ts
@@ -4,14 +4,14 @@ import { Db, MongoClient } from "mongodb";
 export const connectDB = async (): Promise<Db> => {
 
 
-  const uri: string = process.env.MONGO_URL   || "mongodb+srv://Picard:engage@mongomake.3ta2r.mongodb.net/MongoMake?retryWrites=true&w=majority";
+  const uri: string = process.env.MONGO_URL || "mongodb+srv://abc:xyz@123.mongodb.net/?w=majority";
   if(!uri) throw new Error("No mongo url");
   const client = new MongoClient(uri);
 console.log(uri);
   try {
       await client.connect();
       console.info("MongoDB connected");
-      const db = await client.db("test");
+      const db = await client.db("Wiki");
       return db;
    } catch (e) {
       throw e;
